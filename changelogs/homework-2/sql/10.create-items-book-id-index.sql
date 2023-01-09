460ef4994a9e9417701ad12767c60938b7d5add8
Misha Stoma <mishastoma57@gmail.com>
2023-01-09 14:11:52 +0300

    git revert

--- changelogs/homework-2/sql/10.create-items-book-id-index.sql
+++
@@ -1,1 +0,0 @@
-create index idx_fk_book_id_items on items(fk_book_id);
--- changelogs/homework-2/sql/22.select-books-join-items-amount.sql
+++
@@ -1,5 +0,0 @@
-select books.title, count(*) as amount
-from books
-join items
-on books.id = items.fk_book_id 
-group by books.title;
