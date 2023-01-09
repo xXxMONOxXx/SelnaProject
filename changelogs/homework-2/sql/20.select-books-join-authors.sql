db7dc240fd999767b32678c68df754b32e2a95da
Misha Stoma <mishastoma57@gmail.com>
2023-01-09 14:57:27 +0300

    Delete select and added more indexes

--- changelogs/homework-2/sql/20.select-books-join-authors.sql
+++
@@ -1,5 +0,0 @@
-select books
-from books
-join book_authors
-on book_authors.fk_book_id = books.id
-where book_authors.fk_author_id = 1;
--- changelogs/homework-2/sql/21.select-books-join-genres.sql
+++
@@ -1,5 +0,0 @@
-select books
-from books
-join book_genres
-on book_genres.fk_book_id = books.id
-where book_genres.fk_genre_id = 9;
